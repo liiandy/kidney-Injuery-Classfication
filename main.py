88e8084743924550022b87f93133b9180c8f7cea
liiandy <79617546+liiandy@users.noreply.github.com>
2025-04-21 18:45:04 +0800

    Change into 3 class

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ from torch.utils.data import DataLoader
 from dataset import KidneyDataset, get_transform
 from train import train_model
 
-# 🔷 1. 讀取 CSV 檔案
+# 🔻 1. 讀取 CSV 檔案
 csv_file = "/workspace/data/a313112015/rsna_train_new_v2.csv"
 df = pd.read_csv(csv_file)
 
 
 base_path = "/workspace/data/a313112015/BiomedParse/out_test/nifti/"
 
-# 新增一檔 'mask_path' 到 df
+# 新增一欄 'mask_path' 到 df
 df['mask_path'] = df['file_paths'].apply(lambda x: os.path.join(base_path, "mask", *x.split("/")[-3:]))
 
 
 df = df[df['mask_path'].apply(os.path.exists)].reset_index(drop=True)
 print(f"✅ Total samples with mask found: {len(df)}")
 
-# 🔷 6:3:1 分割
+# 🔻 6:3:1 分割
 train_df, temp_df = train_test_split(
     df,
     test_size=0.4,  # 驗證 + 測試
@@ -46,10 +46,12 @@ def data_progress(df, dicts):
         # gt = f'/tf/angela0503/Spleen_data/RAS_dilation_10/{row["spleen_injury"]}_{row["chartNo"]}@venous.nii.gz' 
         mask = row["mask_path"]
         
-        if row['kidney_healthy'] == 1:
-            label = 0
-        else:
+        if row['kidney_low'] == 1:
             label = 1
+        elif row['kidney_high'] == 1:
+            label = 2  
+        else:
+            label = 0
 
         dicts.append({'image':os.path.basename(image), 'image_path':image, 'mask_path':mask, 'label':label})
     return dicts
@@ -74,4 +76,4 @@ train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
 valid_loader = DataLoader(valid_dataset, batch_size=8, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False)
 
-train_model(train_loader, valid_loader, num_classes=2, num_epochs=10)
+train_model(train_loader, valid_loader, num_classes=3, num_epochs=10)
